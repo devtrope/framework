639deb5a39282b46177da7cc3f1eba03fd66419d
Quentin SCHIFFERLE <q.schifferle@tod.fr>
2025-10-24 11:51:55 +0200

    feat: keep routes in cache in production

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -6,6 +6,23 @@ class Application
 {
     public static function init(\Ludens\Http\Request $request)
     {
+        if ($_ENV['APP_ENVIRONMENT'] === 'production') {
+            $cacheFile = CACHE_PATH . '/routes.php';
+
+            if (! file_exists($cacheFile)) {
+                if (! file_exists(ROUTES_PATH)) {
+                    throw new \Exception('Routes file not found at ' . ROUTES_PATH);
+                }
+                
+                require ROUTES_PATH;
+
+                \Ludens\Routing\Route::cache();
+            }
+
+            $routes = require $cacheFile;
+            \Ludens\Routing\Route::load($routes);
+        }
+
         \Ludens\Routing\Router::dispatch($request);
     }
 }
--- src/Routing/Route.php
+++ src/Routing/Route.php
@@ -21,6 +21,28 @@ class Route
         self::$routes[$method][$path] = $handler;
     }
 
+    /**
+     * Keeps the routes in a cache file (only in production).
+     * @return void
+     */
+    public static function cache(): void
+    {
+        $export = var_export(self::$routes, true);
+        $cacheFile = CACHE_PATH . '/routes.php';
+        $content = "<?php\n\nreturn " . $export . ";\n";
+        file_put_contents($cacheFile, $content);
+    }
+
+    /**
+     * Load the routes from a cached file (only in production).
+     * @param array $routes
+     * @return void
+     */
+    public static function load(array $routes): void
+    {
+        self::$routes = $routes;
+    }
+
     public static function list(string $requestMethod): array
     {
         if (! isset(self::$routes[$requestMethod])) {
